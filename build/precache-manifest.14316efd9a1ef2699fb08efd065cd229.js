50e883197106e2facd9a87810cf1fd26f47b562a
Isco <Iscocuesta@hotmail.com>
2019-07-20 14:34:34 -0500

    user pages saved

---
+++ build/precache-manifest.14316efd9a1ef2699fb08efd065cd229.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b085a8205af6bbce6a1dca4bf84fff",
+    "url": "/spiderVer-Memory/index.html/index.html"
+  },
+  {
+    "revision": "f15e047e4e29f0006379",
+    "url": "/spiderVer-Memory/index.html/static/css/main.048f2b7e.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/spiderVer-Memory/index.html/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "f15e047e4e29f0006379",
+    "url": "/spiderVer-Memory/index.html/static/js/main.71b7952e.chunk.js"
+  },
+  {
+    "revision": "a4a7f51e6c084ad545e1",
+    "url": "/spiderVer-Memory/index.html/static/js/runtime~main.05102bc4.js"
+  }
+]);
