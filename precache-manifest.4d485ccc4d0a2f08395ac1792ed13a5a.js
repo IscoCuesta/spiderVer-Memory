cfd97c110a742c62c4cde1ca9a0255fd9e9b1d05
Isco <Iscocuesta@hotmail.com>
2019-07-22 11:40:50 -0500

    Updates

---
+++ precache-manifest.4d485ccc4d0a2f08395ac1792ed13a5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0543033cdb7c0c98b59f2bc11fe79a28",
+    "url": "/spiderVer-Memory/index.html"
+  },
+  {
+    "revision": "7b91deb187abaee3f786",
+    "url": "/spiderVer-Memory/static/css/main.0a5547f6.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/spiderVer-Memory/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "7b91deb187abaee3f786",
+    "url": "/spiderVer-Memory/static/js/main.71b7952e.chunk.js"
+  },
+  {
+    "revision": "bd176da0cd5b2dd1bb5c",
+    "url": "/spiderVer-Memory/static/js/runtime~main.be229f71.js"
+  }
+]);
